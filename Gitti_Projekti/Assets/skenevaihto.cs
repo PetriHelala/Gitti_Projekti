e9d444f2a2d29a13ce3fd6020d7d896f98bf09e1
Petri <zenid@windowslive.com>
2022-05-09 14:34:08 +0300

    Gitti Projekti skene vaihto skripti ja "Scene1"

---
+++ Gitti_Projekti/Assets/skenevaihto.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class skenevaihto : MonoBehaviour
+{
+    
+    public void LataaSkene2(string sceneName) 
+    {
+        
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+
+}
